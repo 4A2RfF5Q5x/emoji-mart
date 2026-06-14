11a5cc81803f757fb038bf5bb49cc9cad07fee5b
VBasko <vvbasko@outlook.com>
2026-06-14 20:24:14 -0300

    feat(@emoji-mart/vue): implement EmojiPicker component

---
+++ packages/emoji-mart-vue/vue.ts
@@ -0,0 +1,26 @@
+// @ts-nocheck
+import { defineComponent, h, ref, watch, onMounted, onBeforeUnmount } from 'vue'
+import { Picker } from 'emoji-mart'
+
+export default defineComponent({
+  name: 'EmojiPicker',
+  inheritAttrs: false,
+  setup(_, { attrs }) {
+    const el = ref(null)
+    const instance = ref(null)
+
+    onMounted(() => {
+      instance.value = new Picker({ ...attrs, ref: { current: el.value } })
+    })
+
+    watch(() => ({ ...attrs }), (newAttrs) => {
+      instance.value?.update(newAttrs)
+    }, { deep: true })
+
+    onBeforeUnmount(() => {
+      instance.value = null
+    })
+
+    return () => h('div', { ref: el })
+  },
+})
